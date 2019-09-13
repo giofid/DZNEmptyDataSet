c8bac77aec5b73b94ea012888063e57830f0b303
Giorgio Fiderio <giorgio.fiderio@staff.aruba.it>
2019-09-13 14:56:36 +0200

    Add vertical anchors Add new enum to allows for vertically anchoring the empty view to the top, center, or bottom of the container.

--- Source/UIScrollView+EmptyDataSet.h
+++ Source/UIScrollView+EmptyDataSet.h
@@ -12,6 +12,12 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_ENUM (NSUInteger, DZNEmptyDataSetVerticalAnchorLocation) {
+    DZNEmptyDataSetVerticalAnchorLocationCenter = 0,
+    DZNEmptyDataSetVerticalAnchorLocationTop,
+    DZNEmptyDataSetVerticalAnchorLocationBottom
+};
+
 @protocol DZNEmptyDataSetSource;
 @protocol DZNEmptyDataSetDelegate;
 
@@ -154,6 +160,14 @@ NS_ASSUME_NONNULL_BEGIN
  @return The space height between elements.
  */
 - (CGFloat)spaceHeightForEmptyDataSet:(UIScrollView *)scrollView;
+
+/**
+ Asks the data source for a vertical anchor location. Default is DZNEmptyDataSetVerticalAnchorLocationCenter.
+ 
+ @param scrollView A scrollview subclass object informing the data source.
+ @return A DZNEmptyDataSetVerticalAnchorLocation value indicating if the view should be pinned to the top, center, or bottom of its container.
+ */
+- (DZNEmptyDataSetVerticalAnchorLocation)verticalAnchorLocationForEmptyDataSet:(UIScrollView *)scrollView;
 
 @end
 
